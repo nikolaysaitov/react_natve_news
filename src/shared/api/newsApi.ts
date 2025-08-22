2546f3e1308757049777f0ce452dbc6ec49ca091
nikolay <saitov@impermebel.ru>
2025-08-22 09:10:16 +0300

    second

--- src/shared/api/newsApi.ts
+++ src/shared/api/newsApi.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import {NewsResponse, NewsApiParams} from '../../entities/news/types';
+import Config from 'react-native-config';
 
-const API_KEY = '49395fb2b3e749a19a3411d320719f10'; // Замените на ваш ключ
 const BASE_URL = 'https://newsapi.org/v2';
 
 export const newsApi = {
   getTopHeadlines: async (params: NewsApiParams): Promise<NewsResponse> => {
     try {
+      console.log('Config.NEWS_API_KEY', Config);
       const response = await axios.get(`${BASE_URL}/top-headlines`, {
         params: {
           ...params,
-          apiKey: API_KEY,
+          apiKey: Config.NEWS_API_KEY,
         },
       });
       return response.data;
